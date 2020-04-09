b41ff29db2982c8392b8c56eaa41145e4f6c8a22
nosemasashi <ma.nose9413@gmail.com>
2020-04-09 09:59:35 +0900

    AdministratorRepositoryを作成しました。

---
+++ src/main/java/jp/co/sample/domain/AdministratorRepository.java
@@ -0,0 +1,88 @@
+package jp.co.sample.domain;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+/**
+ * administratorsテーブルを操作するリポジトリ.
+ * 
+ * @author masashi.nose
+ *
+ */
+@Repository
+public class AdministratorRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	/** administratorsテーブル１行分の情報を持つローマッパー */
+	private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER = (rs, i) -> {
+		Administrator administrator = new Administrator();
+		administrator.setId(rs.getInt("id"));
+		administrator.setName(rs.getString("name"));
+		administrator.setMailAddress(rs.getString("mail_address"));
+		administrator.setPassword(rs.getString("password"));
+		return administrator;
+	};
+
+	/** IDが自動採番されたオブジェクトを返すインサートの準備 */
+	private SimpleJdbcInsert insert;
+
+	@PostConstruct
+	public void init() {
+		SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert((JdbcTemplate) template.getJdbcOperations());
+		SimpleJdbcInsert withTableName = simpleJdbcInsert.withTableName("administrators");
+		insert = withTableName.usingGeneratedKeyColumns("id");
+	}
+
+	/**
+	 * 
+	 * administratorsテーブルに管理者情報をインサートします.
+	 * 
+	 * @param administrator 管理者情報
+	 * @return 自動採番されたIDを持つ管理者情報
+	 */
+	public Administrator insert(Administrator administrator) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
+
+		if (administrator.getId() == null) {
+			Number key = insert.executeAndReturnKey(param);
+			administrator.setId(key.intValue());
+			System.out.println(key + "が割り当てられました。");
+
+		}
+		return administrator;
+	}
+
+	/**
+	 * 
+	 * メールアドレスとパスワードから登録されている管理者情報を検索します
+	 * 
+	 * @param mailAddress メールアドレス
+	 * @param password    パスワード
+	 * @return 管理者情報（未登録の場合はnullが返ります）
+	 */
+	public Administrator findByEmailAndPassword(String mailAddress, String password) {
+		String sql = "SELECT id, name, mail_address, password FROM administrators WHERE mail_address = :mail_address AND password = :password";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("mail_address", mailAddress)
+				.addValue("password", password);
+		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
+
+		if (administratorList.size() == 0) {
+			return null;
+		} else
+			return administratorList.get(0);
+	}
+
+}
